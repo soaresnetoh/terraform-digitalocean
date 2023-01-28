eb4721420960ace00b963b1cdac6a2959d167a0e
Hernani Soares <soaresnetoh@gmail.com>
2023-01-28 12:05:13 -0300

    Versions kubernetes update down - yml kubedev

--- k8s.tf
+++ k8s.tf
@@ -2,7 +2,8 @@ resource "digitalocean_kubernetes_cluster" "k8s" {
   name   = "k8s"
   region = var.region
   # Grab the latest version slug from `doctl kubernetes options versions`
-  version = "1.25.4-do.0"
+  version = "1.24.8-do.0"
+  # version = "1.25.4-do.0"
 
   node_pool {
     name       = "default"
